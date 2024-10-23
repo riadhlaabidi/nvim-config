3688c795d8287cfa6bb30d0a8fca72b74179fe5d
riadhlaabidi <riadh.by.laabidi@gmail.com>
2024-10-24 00:22:31 +0100

    Update theme

--- init.lua
+++ init.lua
@@ -16,6 +16,9 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+vim.opt.termguicolors = true
+vim.cmd.colorscheme 'default'
+
 -- Make line numbers default
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
